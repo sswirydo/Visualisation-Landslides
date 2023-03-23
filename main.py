5d07d32a0580109c39c2ff7c3d1ffc5c096403ce
Alexinator <58421183+Alexinator@users.noreply.github.com>
2023-03-23 13:07:21 +0100

    add images jsp pq ca bug :)) :-1:

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from dash import html
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 import plotly.express as px
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, ALL
 import preprocess as prep
 import dash_leaflet as dl
+from dash.exceptions import PreventUpdate
 
 from shapely.geometry import Point
+import json
 from datetime import datetime
 
 app = dash.Dash(__name__, title='Landslides',
@@ -70,16 +72,18 @@ app.layout = html.Div([
             ], style={'align': "center"})
         ]),
         dl.Map(
-            [
+            children=[
                 dl.TileLayer(),
                 dl.MarkerClusterGroup(
-                    id='map'
+                    html.Div(id='placeholder', hidden=True),
+                    id='markers'
                 )
             ],
             style={'width': '100%', 'height': '50vh',
                    'margin': "auto", "display": "block"},
             center=[51.5074, -0.1278],
             zoom=10,
+            id='map'
         ),
         # dcc.Graph(id='map', style={'height': '90vh'}, figure=dict(layout=dict(
         #    autosize=True)), config=dict(responsive=True, displayModeBar=False))
@@ -118,7 +122,7 @@ app.layout = html.Div([
 """
 
 
-@app.callback(Output('map', 'children'),
+@app.callback(Output('markers', 'children'),
               Input('dropdown', 'value'),
               Input('datepickerrange', 'start_date'),
               Input('datepickerrange', 'end_date'))
@@ -135,19 +139,33 @@ def update_figure(selected_value, start_date, end_date):
     filtered_df['fatality_count'] = filtered_df['fatality_count'].fillna(0)
     markers = [
         dl.Marker(
+            id={'type': 'marker', 'index': str(row['event_id'])},
             position=[row['latitude'], row['longitude']],
-            children=dl.Tooltip(
-                html.Div([
-                    html.Img(src=row['photo_link'], style={
-                             "width": "50px", "height": "50px"}),
-                    html.H3(row['event_title'], style={
-                            "color": "darkblue", "overflow-wrap": "break-word"}),
-                    html.P(row['event_description'],),
-                    html.P(row['source_name']),
-                ], style={'width': '100px', 'white-space': 'normal'}))
+            children=[
+                dl.Tooltip(
+                    html.Div([
+                        html.Img(src=row['photo_link'], style={
+                            "width": "50px", "height": "50px"}),
+                        html.H3(row['event_title'], style={
+                                "color": "darkblue", "overflow-wrap": "break-word"}),
+                        html.P(row['event_description'],),
+                        html.P(row['source_name']),
+                    ], style={'width': '300px', 'white-space': 'normal'})),
+
+                dl.Popup(
+                    html.Div([
+                        html.Img(src=row['photo_link'], style={
+                            "width": "50px", "height": "50px"}),
+                        html.H3(row['event_title'], style={
+                                "color": "darkblue", "overflow-wrap": "break-word"}),
+                        html.P(row['event_description'],),
+                        html.P(row['source_name']),
+                    ], style={'width': '300px', 'white-space': 'normal'}))
+            ]
         )
         for i, row in filtered_df.iterrows()
     ]
+    print(markers[0], markers[1])
     return markers
 
 
@@ -156,6 +174,19 @@ def update_figure(selected_value, start_date, end_date):
                Input('datepickerrange', 'end_date'))
 def update_output_datepicker(start_date, end_date):
     return str(start_date)
+
+
+@ app.callback(Output("placeholder", "children"),
+               [Input({'type': 'marker', 'index': ALL}, 'n_clicks')])
+def marker_click(args):
+    if not any(args):
+        print('no args')
+        return None
+    value = dash.callback_context.triggered[0]['value']
+    marker_id = json.loads(
+        dash.callback_context.triggered[0]['prop_id'].split(".")[0])["index"]
+    print(marker_id)
+    return None
 
 
 """
